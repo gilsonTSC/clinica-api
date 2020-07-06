af6e3c9953b3bc1651a4ee7c48a5e61fff5d9d6a
Gilson <gilson.costa@dce.ufpb.br>
2020-07-06 20:34:04 -0300

    Refatorando o DTO procedimento

--- src/main/java/com/gilsontsc/clinica/api/dto/ProcedimentoDTO.java
+++ src/main/java/com/gilsontsc/clinica/api/dto/ProcedimentoDTO.java
@@ -5,6 +5,8 @@ import javax.validation.constraints.NotNull;
 import org.hibernate.validator.constraints.Length;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.gilsontsc.clinica.api.entity.Consulta;
+import com.gilsontsc.clinica.api.entity.Medico;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -18,7 +20,11 @@ import lombok.NoArgsConstructor;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ProcedimentoDTO {
 
-private Long id;
+	private Long id;
+	
+	private Medico medico;
+	
+	private Consulta consulta;
 	
 	@NotNull(message = "Informe o nome do procedimento")
 	@Length(min=3, max=50, message="O nome deve conter entre 3 a 50 caracteres")
