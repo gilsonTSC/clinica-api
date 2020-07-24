cb70a0ebf73ae1cc4ea06ce20a4f174f8edac3aa
Gilson <gilson.costa@dce.ufpb.br>
2020-07-24 19:06:31 -0300

    Habilitando o CORS na API

---
+++ src/main/java/com/gilsontsc/clinica/api/config/WebConfig.java
@@ -0,0 +1,14 @@
+package com.gilsontsc.clinica.api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class WebConfig {
+
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+			.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+	}
+	
+}
