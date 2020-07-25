a7765052eaba24d466f708f4d4aeb3d9f6d534fe
Gilson <gilson.costa@dce.ufpb.br>
2020-07-25 09:33:34 -0300

    Implementando as exceções personalizadas

--- src/main/java/com/gilsontsc/clinica/api/controller/exception/handler/ControllerExceptionHandler.java
+++ src/main/java/com/gilsontsc/clinica/api/controller/exception/handler/ControllerExceptionHandler.java
@@ -4,9 +4,11 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.gilsontsc.clinica.api.controller.exception.InvalidJwtAuthenticationException;
 import com.gilsontsc.clinica.api.controller.exception.ObjectNotFoundException;
 
 @ControllerAdvice
@@ -14,6 +16,18 @@ public class ControllerExceptionHandler {
 
 	@ExceptionHandler(ObjectNotFoundException.class)
 	private ResponseEntity<StandarError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request){
+		StandarError err = new StandarError(System.currentTimeMillis(), HttpStatus.NOT_FOUND.value(), "Não encontrado", e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+	}
+
+	@ExceptionHandler(InvalidJwtAuthenticationException.class)
+	private ResponseEntity<StandarError> InvalidJwtAuthentication(InvalidJwtAuthenticationException e, HttpServletRequest request){
+		StandarError err = new StandarError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(), "Não encontrado", e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+	}
+	
+	@ExceptionHandler(UsernameNotFoundException.class)
+	private ResponseEntity<StandarError> UsernameNotFound(UsernameNotFoundException e, HttpServletRequest request){
 		StandarError err = new StandarError(System.currentTimeMillis(), HttpStatus.NOT_FOUND.value(), "Não encontrado", e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
 	}
